ce644d1e2226a836f80a697720a3e548ada28388
Jonathan Mayol <mayoljonathan@gmail.com>
2025-05-13 23:51:54 +0800

    feat: added SearchDto and enable global validation

--- apps/backend-api/src/search/search.controller.ts
+++ apps/backend-api/src/search/search.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, NotImplementedException, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  NotImplementedException,
+  Post,
+} from '@nestjs/common';
+import { SearchDto } from './dto/search.dto';
 
 @Controller()
 export class SearchController {
   @Post('/execute')
-  search() {
+  search(@Body() data: SearchDto) {
     throw new NotImplementedException();
   }
 }
