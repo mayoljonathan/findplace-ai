4085b4c53ca31e2d9528f858efa85754d070c2b3
Jonathan Mayol <mayoljonathan@gmail.com>
2025-05-15 01:29:53 +0800

    feat: added search_action to foursquare category id

--- apps/backend-api/src/features/search/constants/search-action.ts
+++ apps/backend-api/src/features/search/constants/search-action.ts
@@ -2,4 +2,8 @@ export const SEARCH_ACTIONS = {
   restaurant_search: 'restaurant_search',
 } as const;
 
+export const SEARCH_ACTION_TO_FOURSQUARE_CATEGORY = {
+  [SEARCH_ACTIONS.restaurant_search]: '4d4b7105d754a06374d81259',
+} as const;
+
 export type SearchAction = keyof typeof SEARCH_ACTIONS;
