ee9176ea5d0b20cf717fa736fbb290849c1ddb9c
Jonathan Mayol <mayoljonathan@gmail.com>
2025-05-15 03:22:11 +0800

    chore: update prompt

--- apps/backend-api/src/services/open-ai/constants/prompts.ts
+++ apps/backend-api/src/services/open-ai/constants/prompts.ts
@@ -19,11 +19,10 @@ Expected JSON output:
 ${restaurantSearchCommandExample.output}
 
 The only valid actions you must able to respond with JSON object are:
-[${Object.keys(SEARCH_ACTIONS).join(', ')}]
+${Object.keys(SEARCH_ACTIONS).join(', ')}
 
 Notes:
 - You must respond either JSON object only or null. Don't prefix the returned value with \`\`\`json\`\`\` or \`\`\` or anything else.
-- The actions must be conciding with the user's message. If the user's message is not related to the specified actions, respond with null.
-- You respond with the JSON object only if you understand the user's message and fill-up each properties in the SearchCommand interface. Otherwise, respond with null.
+- The action must be conciding with the user's message. If the user's message is not related to the specified action, respond with null.
 - The required properties in the SearchCommand interface must be filled-up. If one required properties is lacking, return null. The optional properties should always be available and set its value to null if you can't extract its value from the user's message.
 `;
