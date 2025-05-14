cdbbf2f66f3b22071b0a687bec3ec0bcd8890c6a
Jonathan Mayol <mayoljonathan@gmail.com>
2025-05-15 04:16:52 +0800

    wip: initial layout for home

---
+++ apps/frontend-web/src/components/common/PlaceList/PlaceListItem.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface PlaceListItemProps {}
+
+export const PlaceListItem: React.FC<PlaceListItemProps> = (props) => {
+  const {} = props;
+
+  return <div>PlaceListItem</div>;
+};
+
+PlaceListItem.displayName = "PlaceListItem";
