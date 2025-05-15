d282060adb0b388ea60259a5ae2462108cc2d993
Jonathan Mayol <mayoljonathan@gmail.com>
2025-05-15 17:58:55 +0800

    feat: added proxy pass

--- apps/frontend-web/next.config.ts
+++ apps/frontend-web/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  rewrites: async () => {
+    const baseUrl = process.env.NEXT_PUBLIC_API_URL;
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${baseUrl}/api/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
