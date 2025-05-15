93ffe599e2cbbccf057d995d8e5458ea58d10bd6
Jonathan Mayol <mayoljonathan@gmail.com>
2025-05-15 19:51:06 +0800

    feat: added HttpService class with response error interceptor

---
+++ apps/frontend-web/src/service/http.ts
@@ -0,0 +1,29 @@
+import axios, { Axios, AxiosError, AxiosRequestConfig } from "axios";
+import { ApiError } from "../types";
+
+export class HttpService extends Axios {
+  constructor(config?: AxiosRequestConfig) {
+    super(config);
+    const instance = axios.create(config);
+
+    instance.interceptors.response.use(
+      (response) => response,
+      (error: AxiosError) => {
+        if (
+          typeof error.response?.data === "object" &&
+          "error" in (error.response?.data as ApiError)
+        ) {
+          return Promise.reject(error.response?.data as ApiError);
+        }
+
+        return Promise.reject({
+          error: error.name,
+          message: error.message,
+          statusCode: error.response?.status!,
+        } satisfies ApiError);
+      }
+    );
+
+    return instance;
+  }
+}
