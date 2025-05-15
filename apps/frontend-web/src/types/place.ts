3b01c3427773ea2708e52010ff1ad66fc4617f51
Jonathan Mayol <mayoljonathan@gmail.com>
2025-05-15 19:50:33 +0800

    feat: added Place type

---
+++ apps/frontend-web/src/types/place.ts
@@ -0,0 +1,112 @@
+export interface Coordinate {
+  latitude: number;
+  longitude: number;
+}
+
+export interface PlaceTip {
+  id: string;
+  created_at: Date;
+  text: string;
+  url: string;
+  photo?: string;
+  lang: string;
+  agree_count: number;
+  disagree_count: number;
+}
+
+export interface PlaceHour {
+  close: string;
+  day: number;
+  open: string;
+}
+
+export interface Place {
+  fsq_id: string;
+  categories: {
+    id: number;
+    name: string;
+    short_name: string;
+    plural_name: string;
+    icon: {
+      prefix: string;
+      suffix: string;
+    };
+  }[];
+  chains: {
+    id: string;
+    name: string;
+  }[];
+  closed_bucket: string;
+  date_closed: string;
+  description: string;
+  distance: number;
+  email: string;
+  fax: string;
+  geocodes: Partial<{
+    drop_off: Coordinate;
+    front_door: Coordinate;
+    main: Coordinate;
+    road: Coordinate;
+    roof: Coordinate;
+  }>;
+  hours: {
+    display: string;
+    is_local_holiday: boolean;
+    open_now: boolean;
+    regular: PlaceHour[];
+  };
+  hours_popular: PlaceHour[];
+  link: string;
+  location: Partial<{
+    address: string;
+    address_extended: string;
+    admin_region: string;
+    census_block: string;
+    country: string;
+    cross_street: string;
+    dma: string;
+    formatted_address: string;
+    locality: string;
+    neighborhood: string[];
+    po_box: string;
+    post_town: string;
+    postcode: string;
+    region: string;
+  }>;
+  menu: string;
+  name: string;
+  photos: {
+    id: string;
+    created_at: Date;
+    prefix: string;
+    suffix: string;
+    width: number;
+    height: number;
+    classifications: string[];
+    tip: Omit<PlaceTip, "photo">;
+  }[];
+  popularity: number;
+  price: number;
+  rating: number;
+  related_places: Partial<{
+    parent: string;
+  }>;
+  social_media: {
+    facebook_id: string;
+    instagram: string;
+    twitter: string;
+  };
+  stats: {
+    total_photos: number;
+    total_ratings: number;
+    total_tips: number;
+  };
+  store_id: string;
+  tastes: string[];
+  tel: string;
+  timezone: string;
+  tips: PlaceTip[];
+  venue_reality_bucket: string;
+  verified: boolean;
+  website: string;
+}
