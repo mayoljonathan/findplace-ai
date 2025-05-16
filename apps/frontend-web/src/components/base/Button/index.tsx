5fd2bf9d670da23551dcfc13a785c1ab67e73601
Jonathan Mayol <mayoljonathan@gmail.com>
2025-05-16 20:36:16 +0800

    feat: added isLoading for Button

--- apps/frontend-web/src/components/base/Button/index.tsx
+++ apps/frontend-web/src/components/base/Button/index.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
+import { Loader } from "../Loader";
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
@@ -39,10 +40,15 @@ function Button({
   variant,
   size,
   asChild = false,
-  ...props
+  children,
+  isLoading,
+  iconLeft,
+  ...restProps
 }: React.ComponentProps<"button"> &
   VariantProps<typeof buttonVariants> & {
     asChild?: boolean;
+    isLoading?: boolean;
+    iconLeft?: React.ReactNode;
   }) {
   const Comp = asChild ? Slot : "button";
 
@@ -50,8 +56,15 @@ function Button({
     <Comp
       data-slot="button"
       className={cn(buttonVariants({ variant, size, className }))}
-      {...props}
-    />
+      {...restProps}
+    >
+      {!isLoading ? (
+        <Slot className="size-4">{iconLeft}</Slot>
+      ) : (
+        <Loader className="text-primary-foreground" />
+      )}
+      {children}
+    </Comp>
   );
 }
 
