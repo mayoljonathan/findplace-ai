9c531067ce2fe9db4638383231b7bb726f024a6b
Jonathan Mayol <mayoljonathan@gmail.com>
2025-05-16 22:04:07 +0800

    fix: update message property required error

--- apps/backend-api/src/features/search/dto/search.dto.ts
+++ apps/backend-api/src/features/search/dto/search.dto.ts
@@ -3,7 +3,10 @@ import { Transform } from 'class-transformer';
 
 export class SearchDto {
   @IsString()
-  @IsNotEmpty()
+  @IsNotEmpty({
+    message:
+      'Enter what you are looking for and we will suggest places for you',
+  })
   @Transform(({ value }) => value.trim())
   message: string;
 }
